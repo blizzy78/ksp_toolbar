0cf3738e668c5a5cf780725d37b8b9a2e4a35697
Maik Schreiber <blizzy@blizzy.de>
2014-01-01 04:23:09 +0100

    prepare for release 1.4.0

--- Toolbar/Internal/ToolbarManager.cs
+++ Toolbar/Internal/ToolbarManager.cs
@@ -36,7 +36,7 @@ namespace Toolbar {
 
 		private static readonly string settingsFile = KSPUtil.ApplicationRootPath + "GameData/toolbar-settings.dat";
 
-		private const int VERSION = 3;
+		private const int VERSION = 4;
 
 		private static WWW versionWWW;
 		private static bool? newVersionAvailable = null;
