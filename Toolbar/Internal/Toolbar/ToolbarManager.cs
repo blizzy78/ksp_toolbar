195029f02491a77458afc67d6ec6a9a9d1f05172
Maik Schreiber <blizzy@blizzy.de>
2014-03-03 18:20:57 +0100

    prepare for release 1.7.0

--- Toolbar/Internal/Toolbar/ToolbarManager.cs
+++ Toolbar/Internal/Toolbar/ToolbarManager.cs
@@ -36,7 +36,7 @@ namespace Toolbar {
 		internal const string FORUM_THREAD_URL = "http://forum.kerbalspaceprogram.com/threads/60863";
 		internal const string DONATE_URL = "https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PCRP5Y2MUS62A";
 		internal const string NAMESPACE_INTERNAL = "__TOOLBAR_INTERNAL";
-		internal const int VERSION = 15;
+		internal const int VERSION = 16;
 
 		internal static ToolbarManager InternalInstance;
 
