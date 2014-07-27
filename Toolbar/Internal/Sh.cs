21d7f10ebbf8db8eea215418abd5ba4c6526917a
Maik Schreiber <blizzy@blizzy.de>
2014-07-27 18:26:52 +0200

    optimize

--- Toolbar/Internal/Sh.cs
+++ Toolbar/Internal/Sh.cs
@@ -34,31 +34,44 @@ using UnityEngine;
 namespace Toolbar {
 	[KSPAddonFixed(KSPAddon.Startup.MainMenu, false, typeof(Sh))]
 	internal class Sh : MonoBehaviour {
+		private static bool done;
+
 		private GameObject kerbal;
 		private Vector3 kerbalOrigPos;
 		private Quaternion kerbalOrigRot;
 #if DEBUG
 		private Rect windowRect = new Rect(50, 50, 0, 0);
 		private GameObject gameObjectToPosition;
-		private Vector3 newPosOffset;
+		private Vector3 gameObjectToPositionOrigPos;
+		private Quaternion gameObjectToPositionOrigRot;
 		private Vector3 posOffset = Vector3.zero;
-		private Vector3 rotationOffset = Vector3.zero;
+		private Vector3 rotation = Vector3.zero;
+		private float scale = 1;
 #endif
 
 		internal void Start() {
+			if (done) {
+				return;
+			}
+
 			kerbal = GameObject.Find("kbEVA@idle");
 			if (kerbal == null) {
 				return;
 			}
 
+#if !DEBUG
 			if (ToolbarManager.InternalInstance.UpdateChecker.Done) {
 				UnityEngine.Random.seed = (int) DateTime.UtcNow.Ticks;
 				float random = UnityEngine.Random.Range(0f, 100f);
 				Log.debug("sh: {0:F1} vs. {1:F1}", random, ToolbarManager.InternalInstance.UpdateChecker.Sh);
 				if (random < ToolbarManager.InternalInstance.UpdateChecker.Sh) {
+#endif
 					sh();
+					done = true;
+#if !DEBUG
 				}
 			}
+#endif
 		}
 
 		private void sh() {
@@ -89,10 +102,34 @@ namespace Toolbar {
 			doKerbal(new Vector3(18.54f, -0.34f, 3.96f), 160);
 			doKerbal(new Vector3(12.96f, 0.1f, -0.3f), 0);
 
+			GameObject rcsTank = GameDatabase.Instance.GetModel("Squad/Parts/FuelTank/RCSFuelTank/model");
+			if (rcsTank != null) {
+				rcsTank.transform.position = new Vector3(16.67f, -0.89f, 4.32f);
+				rcsTank.transform.rotation = Quaternion.Euler(2, 134, 0);
+				rcsTank.transform.localScale = Vector3.one / 2f;
+				rcsTank.SetActive(true);
+			}
+
+			GameObject rcsBlock = GameDatabase.Instance.GetModel("Squad/Parts/Utility/RCS block/model");
+			if (rcsBlock != null) {
+				rcsBlock.transform.position = new Vector3(16.56f, -0.69f, 4.38f);
+				rcsBlock.transform.rotation = Quaternion.Euler(3, 35, -89);
+				rcsBlock.transform.localScale = Vector3.one / 2f;
+				rcsBlock.SetActive(true);
+			}
+
+			GameObject rcsBlock2 = GameDatabase.Instance.GetModel("Squad/Parts/Utility/RCS block/model");
+			if (rcsBlock2 != null) {
+				rcsBlock2.transform.position = new Vector3(16.3f, -0.96f, 4.29f);
+				rcsBlock2.transform.rotation = Quaternion.Euler(-49, 25, -44);
+				rcsBlock2.transform.localScale = Vector3.one / 2f;
+				rcsBlock2.SetActive(true);
+			}
+
 #if DEBUG
-			newPosOffset = new Vector3(10, 0, -1);
-			rotationOffset = new Vector3(0, 0, 0);
-			gameObjectToPosition = sh2;
+			//gameObjectToPosition = kerbal2;
+			//gameObjectToPositionOrigPos = gameObjectToPosition.transform.position;
+			//gameObjectToPositionOrigRot = gameObjectToPosition.transform.rotation;
 #endif
 		}
 
@@ -125,40 +162,47 @@ namespace Toolbar {
 
 #if DEBUG
 		private void OnGUI() {
-			windowRect = GUILayout.Window(1, windowRect, id => drawWindow(), "Positioning");
+			if (gameObjectToPosition != null) {
+				windowRect = GUILayout.Window(1, windowRect, id => drawWindow(), "Positioning");
+			}
 		}
 
 		private void drawWindow() {
 			GUILayout.BeginVertical();
 
 			GUILayout.BeginHorizontal();
-			posOffset.x = GUILayout.HorizontalSlider(posOffset.x, -3, 3, GUILayout.Width(300));
-			GUILayout.Label(string.Format("x: {0:F2}", newPosOffset.x + posOffset.x));
+			posOffset.x = GUILayout.HorizontalSlider(posOffset.x, -1000, 1000, GUILayout.Width(400));
+			GUILayout.Label(string.Format("x: {0:F2}", gameObjectToPosition.transform.position.x));
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			posOffset.y = GUILayout.HorizontalSlider(posOffset.y, -1, 1, GUILayout.Width(300));
-			GUILayout.Label(string.Format("y: {0:F2}", newPosOffset.y + posOffset.y));
+			posOffset.y = GUILayout.HorizontalSlider(posOffset.y, -10, 10, GUILayout.Width(400));
+			GUILayout.Label(string.Format("y: {0:F2}", gameObjectToPosition.transform.position.y));
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			posOffset.z = GUILayout.HorizontalSlider(posOffset.z, -3, 3, GUILayout.Width(300));
-			GUILayout.Label(string.Format("z: {0:F2}", newPosOffset.z + posOffset.z));
+			posOffset.z = GUILayout.HorizontalSlider(posOffset.z, -500, 500, GUILayout.Width(400));
+			GUILayout.Label(string.Format("z: {0:F2}", gameObjectToPosition.transform.position.z));
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			rotationOffset.z = GUILayout.HorizontalSlider(rotationOffset.z, -179, 180, GUILayout.Width(300));
-			GUILayout.Label(string.Format("rotZ: {0:F0}", rotationOffset.z));
+			rotation.z = GUILayout.HorizontalSlider(rotation.z, -179, 180, GUILayout.Width(400));
+			GUILayout.Label(string.Format("rotZ: {0:F0}", rotation.z));
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			rotationOffset.x = GUILayout.HorizontalSlider(rotationOffset.x, -179, 180, GUILayout.Width(300));
-			GUILayout.Label(string.Format("rotX: {0:F0}", rotationOffset.x));
+			rotation.x = GUILayout.HorizontalSlider(rotation.x, -179, 180, GUILayout.Width(400));
+			GUILayout.Label(string.Format("rotX: {0:F0}", rotation.x));
 			GUILayout.EndHorizontal();
 
 			GUILayout.BeginHorizontal();
-			rotationOffset.y = GUILayout.HorizontalSlider(rotationOffset.y, -179, 180, GUILayout.Width(300));
-			GUILayout.Label(string.Format("rotY: {0:F0}", rotationOffset.y));
+			rotation.y = GUILayout.HorizontalSlider(rotation.y, -179, 180, GUILayout.Width(400));
+			GUILayout.Label(string.Format("rotY: {0:F0}", rotation.y));
+			GUILayout.EndHorizontal();
+
+			GUILayout.BeginHorizontal();
+			scale = GUILayout.HorizontalSlider(scale, 1, 200, GUILayout.Width(400));
+			GUILayout.Label(string.Format("scale: {0:F0}", scale));
 			GUILayout.EndHorizontal();
 
 			GUILayout.EndVertical();
@@ -168,8 +212,9 @@ namespace Toolbar {
 
 		private void Update() {
 			if (gameObjectToPosition != null) {
-				gameObjectToPosition.transform.position = kerbalOrigPos + newPosOffset + posOffset;
-				gameObjectToPosition.transform.rotation = kerbalOrigRot * Quaternion.Euler(rotationOffset);
+				gameObjectToPosition.transform.position = gameObjectToPositionOrigPos + posOffset;
+				gameObjectToPosition.transform.rotation = Quaternion.Euler(rotation);
+				gameObjectToPosition.transform.localScale = Vector3.one * scale;
 			}
 		}
 #endif
